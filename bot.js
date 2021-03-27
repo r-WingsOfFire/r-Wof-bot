fc3f7d84844cae8d9f4870a7f0fd298618f6246d
dindin-glitch <odinbeuchat.ob@gmail.com>
2021-03-28 00:29:02 +0100

    FINALLY SUCCEEDED TO READ THE SPREADSHEET

--- bot.js
+++ bot.js
@@ -4,6 +4,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
+const readline = require('readline');
+const { google } = require('googleapis');
 
 
 var totalMessages = new Map();
@@ -28,6 +30,93 @@ const alltribes = ["skywing", "seawing", "icewing", "nightwing", "sandwing", "mu
 var lastmessage = undefined;
 
 var quoteBusy = false;
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+	if (err) return console.log('Error loading client secret file:', err);
+	// Authorize a client with credentials, then call the Google Sheets API.
+	authorize(JSON.parse(content), readSheet);
+});
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+	const { client_secret, client_id, redirect_uris } = credentials.installed;
+	const oAuth2Client = new google.auth.OAuth2(
+		client_id, client_secret, redirect_uris[0]);
+
+	// Check if we have previously stored a token.
+	fs.readFile(TOKEN_PATH, (err, token) => {
+		if (err) return getNewToken(oAuth2Client, callback);
+		oAuth2Client.setCredentials(JSON.parse(token));
+		callback(oAuth2Client);
+	});
+}
+
+/**
+   * Get and store new token after prompting for user authorization, and then
+   * execute the given callback with the authorized OAuth2 client.
+   * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+   * @param {getEventsCallback} callback The callback for the authorized client.
+   */
+function getNewToken(oAuth2Client, callback) {
+	const authUrl = oAuth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: SCOPES,
+	});
+	console.log('Authorize this app by visiting this url:', authUrl);
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+	rl.question('Enter the code from that page here: ', (code) => {
+		rl.close();
+		oAuth2Client.getToken(code, (err, token) => {
+			if (err) return console.error('Error while trying to retrieve access token', err);
+			oAuth2Client.setCredentials(token);
+			// Store the token to disk for later program executions
+			fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+				if (err) return console.error(err);
+				console.log('Token stored to', TOKEN_PATH);
+			});
+			callback(oAuth2Client);
+		});
+	});
+}
+
+/**
+   * Prints the dragons for each winglet in the spreadsheet:
+   * @see https://docs.google.com/spreadsheets/d/1ml5rjBRytr8ZoTvXPFO0j8819p6KEVxHYTb8rIvrG44/edit
+   * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+   */
+function readSheet(auth) {
+	const sheets = google.sheets({ version: 'v4', auth });
+	sheets.spreadsheets.values.get({
+		spreadsheetId: '1ml5rjBRytr8ZoTvXPFO0j8819p6KEVxHYTb8rIvrG44',
+		range: 'Dorms!A2:K13',
+	}, (err, res) => {
+		if (err) return console.log('The API returned an error: ' + err);
+		const rows = res.data.values;
+		if (rows.length) {
+			console.log('Winglet: Mudwing, Seawing, Rainwing, Nightwing, Sandwing, Skywing, Icewing, Silkwing, Hivewing and Leafwing');
+			rows.map((row) => {
+				console.log(`${row[0]}: ${row[1]}, ${row[2]}, ${row[3]}, ${row[4]}, ${row[5]}, ${row[6]}, ${row[7]}, ${row[8]}, ${row[9]} and ${row[10]}`);
+			});
+		} else {
+			console.log('No data found.');
+		}
+	});
+}
 
 /*
 const searchReddit = function() {
