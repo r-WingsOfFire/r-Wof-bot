b90eeb779dc218b34a8f3b51d61a999938ac1c3e
Odin Beuchat <66629792+dindin-glitch@users.noreply.github.com>
2021-11-27 17:35:27 +0100

    Added heroku avoid sleep

--- bot.js
+++ bot.js
@@ -22,6 +22,11 @@ var token = '';
 var express = require('express');
 var app     = express();
 
+var http = require("http");
+setInterval(function() {
+    http.get("http://r-wof-bot.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
+
 app.set('port', (process.env.PORT || 5000));
 
 // To avoid Heroku $PORT error
