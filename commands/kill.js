78e42e372a91aaa31993de0b13b308a08d138a19
Odin Beuchat <66629792+dindin-nibnib@users.noreply.github.com>
2022-06-16 17:07:27 +0200

    fixed kill command

--- commands/kill.js
+++ commands/kill.js
@@ -21,16 +21,19 @@ module.exports = {
    * @returns nuthin
    */
   async execute(interaction, client) {
-    if (!interaction.member.roles.resolve("795414220707463188"))
+    /* It's checking if the user has the role with the ID 795414220707463188. If they don't, it's
+    sending a message to the user saying that they don't have the permissions to do that. */
+    if (!interaction.member.roles.resolve("795414220707463188")) {
       interaction.reply({
         ephemeral: true,
         content: "You don't have the permissions to do that!",
       });
+      return;
+    }
 
     /* It's sending a message to the user who executed the command. */
     await interaction.reply({
       content: "Logging off!",
-      ephemeral: true,
     });
 
     /* It's destroying the client, logging the user who killed the bot, and exiting the process. */
