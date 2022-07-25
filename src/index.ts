b4badc85f0cfebe8d99536ff22c5bb234c442ac5
Evan <evanrayweigel@outlook.com>
2022-07-24 23:04:55 -0500

    extract loading and starting functionality into methods
    Also removes some obvious comments

--- src/index.ts
+++ src/index.ts
@@ -1,93 +1,16 @@
-import * as fs from "fs";
-import * as path from "path";
 import { Intents } from "discord.js";
 import "reflect-metadata";
-import * as Discord from "discord.js";
 import { config } from "dotenv";
 import { Client } from "./structures/client";
 
 config();
 
-/* It's getting the token from the .env file. */
 const token = process.env.TOKEN;
 
-/* It's checking if the token is undefined. If it is, it exits the process with an exit code of -1. */
 if (token === undefined) {
   process.exit(-1);
 }
 
-// Create a new client instance
-const client = new Client({
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_MESSAGES,
-  ],
-});
+const client = new Client(__dirname);
 
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js"));
-
-/* It's importing all the commands from the commands folder. */
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  // Set a new item in the Collection
-  // With the key as the command name and the value as the exported module
-  client.commands.set(command.data.name, command);
-}
-
-/* It's a listener that will be called when the client is ready. */
-client.once("ready", async () => {
-  console.log("Ready!");
-  client.user?.setActivity("flying dragons!", { type: "WATCHING" });
-});
-
-client.on("interactionCreate", async (interaction) => {
-  /* It's checking if the interaction is a command. If it isn't, it returns. */
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName == "reload") {
-    interaction.reply({
-      embeds: [
-        new Discord.MessageEmbed().setColor("GOLD").setTitle("Reloading!"),
-      ],
-    });
-    const commandsPath = path.join(__dirname, "commands");
-    const commandFiles = fs
-      .readdirSync(commandsPath)
-      .filter((file) => file.endsWith(".js"));
-
-    /* It's importing all the commands from the commands folder. */
-    for (const file of commandFiles) {
-      const filePath = path.join(commandsPath, file);
-      const command = require(filePath);
-      // Set a new item in the Collection
-      // With the key as the command name and the value as the exported module
-      client.commands.set(command.data.name, command);
-    }
-    client.destroy();
-    client.login(token);
-    return;
-  }
-  /* It's getting the command from the client's commands collection. */
-  const command = client.commands.get(interaction.commandName);
-
-  /* It's checking if the command exists. If it doesn't, it returns. */
-  if (!command) return;
-
-  /* It's trying to execute the command, and if it fails, it will reply with an error message. */
-  try {
-    await command.execute(interaction, client);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
-  }
-});
-
-client.login(token);
+client.start(token);
--- src/structures/client.ts
+++ src/structures/client.ts
@@ -1,6 +1,8 @@
 import * as Discord from "discord.js";
 import { SlashCommandBuilder } from "@discordjs/builders";
 import * as Discordx from "discordx";
+import * as fs from "fs";
+import * as path from "path";
 
 interface CommandFunction {
   (
@@ -17,4 +19,86 @@ interface Command {
 export class Client extends Discordx.Client {
   public commands = new Discord.Collection<string, Command>();
   public quoteBusy = false;
+
+  public constructor(private dir: string) {
+    super({
+      intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+      ],
+    });
+  }
+
+  public async load(): Promise<void> {
+    const commandsPath = path.join(this.dir, "commands");
+    const commandFiles = fs
+      .readdirSync(commandsPath)
+      .filter((file) => path.extname(file) === ".js");
+
+    /* It's importing all the commands from the commands folder. */
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, file);
+      const command: Command = await import(filePath);
+      // Set a new item in the Collection
+      // With the key as the command name and the value as the exported module
+      this.commands.set(command.data.name, command);
+    }
+
+    // /* It's a listener that will be called when the client is ready. */
+    // this.once("ready", async () => {
+    //   console.log("Ready!");
+    //   this.user?.setActivity("flying dragons!", { type: "WATCHING" });
+    // });
+
+    // this.on("interactionCreate", async (interaction) => {
+    //   /* It's checking if the interaction is a command. If it isn't, it returns. */
+    //   if (!interaction.isCommand()) return;
+
+    //   if (interaction.commandName == "reload") {
+    //     interaction.reply({
+    //       embeds: [
+    //         new Discord.MessageEmbed().setColor("GOLD").setTitle("Reloading!"),
+    //       ],
+    //     });
+    //     const commandsPath = path.join(__dirname, "commands");
+    //     const commandFiles = fs
+    //       .readdirSync(commandsPath)
+    //       .filter((file) => file.endsWith(".js"));
+
+    //     /* It's importing all the commands from the commands folder. */
+    //     for (const file of commandFiles) {
+    //       const filePath = path.join(commandsPath, file);
+    //       const command = require(filePath);
+    //       // Set a new item in the Collection
+    //       // With the key as the command name and the value as the exported module
+    //       this.commands.set(command.data.name, command);
+    //     }
+    //     this.destroy();
+    //     this.login(token);
+    //     return;
+    //   }
+    //   /* It's getting the command from the client's commands collection. */
+    //   const command = client.commands.get(interaction.commandName);
+
+    //   /* It's checking if the command exists. If it doesn't, it returns. */
+    //   if (!command) return;
+
+    //   /* It's trying to execute the command, and if it fails, it will reply with an error message. */
+    //   try {
+    //     await command.execute(interaction, client);
+    //   } catch (error) {
+    //     console.error(error);
+    //     await interaction.reply({
+    //       content: "There was an error while executing this command!",
+    //       ephemeral: true,
+    //     });
+    //   }
+    // });
+  }
+
+  public async start(token: string): Promise<void> {
+    await this.login(token);
+    await this.load();
+  }
 }
