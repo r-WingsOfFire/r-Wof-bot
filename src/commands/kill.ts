6600242ece3e1d5435675c6cef319436bca2b0f4
Evan <evanrayweigel@outlook.com>
2022-07-25 01:24:28 -0500

    remove permission check

--- src/commands/kill.ts
+++ src/commands/kill.ts
@@ -6,18 +6,6 @@ export default {
     .setName("kill")
     .setDescription("Stops the bot"),
   async execute(interaction, client) {
-    console.log("kill");
-    /* It's checking if the user has the role with the ID 795414220707463188. If they don't, it's
-    sending a message to the user saying that they don't have the permissions to do that. */
-    if (!interaction.member?.roles.cache.has("795414220707463188")) {
-      interaction.reply({
-        ephemeral: true,
-        content: "You don't have the permissions to do that!",
-      });
-
-      return;
-    }
-
     await interaction.reply({
       content: "Logging off!",
     });
