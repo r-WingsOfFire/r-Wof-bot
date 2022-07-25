ad108d9fb45ab85a42800667ba8100e0a3353b25
Evan <evanrayweigel@outlook.com>
2022-07-25 00:40:08 -0500

    Create sunny.ts

---
+++ src/commands/sunny.ts
@@ -0,0 +1,25 @@
+import type { Command } from "../types";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import * as Discord from "discord.js";
+import { quotes } from "../config/quotes.json";
+import { randomInteger } from "util/misc";
+
+const sunnyQuotes = quotes.filter((quote) => quote.character === "Sunny");
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("sunny")
+    .setDescription("sends a nice sunny quote"),
+  async execute(interaction, client) {
+    interaction.reply({
+      embeds: [
+        new Discord.MessageEmbed()
+          .setDescription(
+            `"${sunnyQuotes[randomInteger(0, sunnyQuotes.length)].quote}"`
+          )
+          .setFooter("-Sunny")
+          .setColor("GOLD"),
+      ],
+    });
+  },
+} as Command;
