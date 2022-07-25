a1097468294f1d7862ac392919f09c7b1fa86cf6
Evan <evanrayweigel@outlook.com>
2022-07-25 01:22:30 -0500

    whoops

--- src/commands/kill.ts
+++ src/commands/kill.ts
@@ -3,9 +3,10 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 
 export default {
   data: new SlashCommandBuilder()
-    .setName("issue")
-    .setDescription("Refer to this if you found an issue"),
+    .setName("kill")
+    .setDescription("Stops the bot"),
   async execute(interaction, client) {
+    console.log("kill");
     /* It's checking if the user has the role with the ID 795414220707463188. If they don't, it's
     sending a message to the user saying that they don't have the permissions to do that. */
     if (!interaction.member?.roles.resolve("795414220707463188")) {
