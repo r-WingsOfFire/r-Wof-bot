6111997ab2bcc130fe658647ab74e18b9b2f1f54
Evan <evanrayweigel@outlook.com>
2022-07-25 01:31:12 -0500

    throw an error

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ config();
 const token = process.env.TOKEN;
 
 if (token === undefined) {
-  process.exit(1);
+  throw new Error("Missing token");
 }
 
 const client = new Client(clientOptions as ClientOptions, __dirname);
